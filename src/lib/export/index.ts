ae213fb1fb60b76866086b8160f3f0c0b026c2f0
Jakub Mazur <jakmaz.social@icloud.com>
2026-03-03 20:15:36 +0100

    feat: import file

--- src/lib/export/index.ts
+++ src/lib/export/index.ts
@@ -3,3 +3,4 @@
 export { copyToClipboard, downloadJSON, formatJSON } from "./download";
 export type { PortableCircuit, PortableConnection, PortableNode } from "./formats";
 export { transformFromPortable, transformToPortable } from "./transform";
+export { type ValidationResult, validateCircuit } from "./validate";
