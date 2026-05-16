c28d592c924a3ef5477ec07e0e4d468c96de5cfb
Jakub Mazur <jakmaz.social@icloud.com>
2026-05-16 09:26:46 +0200

    feat(landing): seo improvements

--- apps/landing/astro.config.mjs
+++ apps/landing/astro.config.mjs
@@ -2,12 +2,14 @@
 
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), mdx()],
+  site: "https://gately.dev",
+  integrations: [react(), mdx(), sitemap()],
 
   redirects: {
     "/docs": "/learn/first-circuit",
